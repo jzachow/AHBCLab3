5162bdd675f8353da1c099f9259ec3ac8c3111df
Justin <justin.zachow@gmail.com>
2020-07-16 23:04:21 -0400

    Add program and helper class. Initial completed build

---
+++ AHBCLab3/LabTester.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace AHBCLab3
+{
+    class LabTester
+    {
+
+        private bool TestEvenOrOdd(int x)
+        {
+            return (x % 2) == 0 ? true : false;
+        }
+
+        public bool ValidateInput(string input)
+        {
+            if (int.TryParse(input, out int userNumber))
+            {
+                if (userNumber > 0 && userNumber <= 100)
+                {
+                    return true;
+                }
+            }
+
+            if (input.Equals("y", StringComparison.CurrentCultureIgnoreCase)|| input.Equals("n", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return true;
+            }
+
+            Console.WriteLine("Invalid entry. Please try again");
+            return false;
+        }
+
+        public string ProcessToOutput(int x)
+        {
+            if (TestEvenOrOdd(x))
+            {
+                if (x < 25) return "Even and lass than 25";
+                else if (x <= 60) return "Even";
+                else return $"{x} is Even";
+            }
+            else
+            {
+                return $"{x} is Odd";
+            }
+        }
+    }
+}
--- AHBCLab3/Program.cs
+++ AHBCLab3/Program.cs
@@ -6,7 +6,50 @@ namespace AHBCLab3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LabTester tester = new LabTester();
+
+
+            Console.Write("Hello User! Please enter your name:  ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"Welcome {userName}!");
+
+
+            bool userContinue;
+            do
+            {
+
+                bool validInput;
+                string userInput;
+                do
+                {
+                    Console.Write("\nPlease enter an integer between 1 and 100: ");
+                    userInput = Console.ReadLine();
+                    validInput = tester.ValidateInput(userInput);
+                } while (!validInput);
+
+                Console.WriteLine(tester.ProcessToOutput(int.Parse(userInput)));
+
+
+                do
+                {
+                    Console.WriteLine($"\nWould you like to run the program again, {userName}? (y/n)");
+                    userInput = Console.ReadLine();
+                    validInput = tester.ValidateInput(userInput);
+                } while (!validInput);
+
+
+                userContinue = (userInput.Equals("y", StringComparison.InvariantCultureIgnoreCase) ? true : false);
+                if (!userContinue)
+                {
+                    Console.WriteLine("\n-------------------------------------------");
+                    Console.WriteLine($"Thanks for using my program, {userName}!");
+                    Console.WriteLine("-------------------------------------------");
+                }
+
+
+            } while (userContinue);
         }
     }
 }
+
